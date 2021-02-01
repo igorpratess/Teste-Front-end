fd3e352972555c63e9b6ca83e8794d8192521aec
Igor Prates <igoorprates@hotmail.com.br>
2021-02-01 08:26:30 -0300

    Retirando código

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const app = express();
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/front-end/public'));
 
-const productsRoute = require('./routes/products');
-const newsletterRoute = require('./routes/newsletter');
-
-app.use('/products', productsRoute);
-app.use('/newsletter', newsletterRoute);
 app.get('/', (req, res) => {
     res.render('index.html');
 });
